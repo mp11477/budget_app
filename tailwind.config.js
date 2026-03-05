ac5de8263b817dbfc63e75fde56705fa00f66f34
MP-Dev <44982106+mp11477@users.noreply.github.com>
2026-03-05 13:53:02 -0500

    Add Tailwind npm scripts/config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./**/*.py",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
